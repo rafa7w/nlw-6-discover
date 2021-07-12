1c03c051aa7a88ed5d689ab9303453ffd7cc4f13
Rafael William <rafael.fim@hotmail.com>
2021-07-12 20:28:34 -0300

    rendering room number

--- src/route.js
+++ src/route.js
@@ -12,9 +12,7 @@ route.get('/create-pass', (req, res) => {
     res.render('index', { page : 'create-pass' });
 });
 
-route.get('/room/:room', (req, res) => {
-    res.render('room');
-});
+route.get('/room/:room', RoomController.open);
 
 
 // Implicitamente o index em QuestionController está recebendo REQ e RES
