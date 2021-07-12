69d55d96a2288a4338fccf4c0258811c1c77f1c6
Rafael William <rafael.fim@hotmail.com>
2021-07-12 20:01:25 -0300

    storing room data

--- src/db/init.js
+++ src/db/init.js
@@ -2,7 +2,7 @@ const Database = require('./config');
 
 const initDb = {
     async init() {
-        const db = await Database();
+        const db = await Database();''
 
         await db.exec(`CREATE TABLE rooms (
             id INTEGER PRIMARY KEY,
