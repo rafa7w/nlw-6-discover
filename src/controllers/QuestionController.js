0672a09a573ccfc7f4c8863066e705ebd21a23f6
Rafael William <rafael.fim@hotmail.com>
2021-07-13 18:26:02 -0300

    showing questions in browser

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,13 +1,30 @@
 const Database = require('../db/config');
 
 module.exports = {
-    index(req, res) {
+    async index(req, res) {
+        const db = await Database();
         const roomId = req.params.room;
         const questionId = req.params.question;
         const action = req.params.action;
         const password = req.body.password; // pegar pelo atributo name do input type="password"
 
-        console.log(`room: ${roomId}, question: ${questionId}, action: ${action}, pass: ${password}`);
+        /* Verificar se a senha está correta */
+        // db.get traz um único elemento do banco, mas é preciso garantir que ele vai ser único
+        const verifyRoom = await db.get(`SELECT * FROM rooms WHERE id = ${roomId}`); 
+
+        if (verifyRoom.pass == password) {
+            if (action == "delete") {
+
+                await db.run(`DELETE FROM questions WHERE id = ${questionId}`);
+                
+            } else if (action == "check") {
+                
+                await db.run(`UPDATE questions SET read = 1 WHERE id = ${questionId}`);
+
+            } 
+        };
+
+        res.redirect(`/room/${roomId}`);
     },
 
     async create(req, res) {
